b10c66404d9ad6c0a13a4ed9ef9c9a92e334feff
Iva <cicareviciva@hotmail.com>
2019-05-29 21:50:27 +0200

    Initial Commit

---
+++ PostsList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostsList
+//
+//  Created by Iva Cicarevic on 5/29/19.
+//  Copyright © 2019 Iva Cicarevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
